504b89bca55cc33ebf79c51fc9c323eabaf29685
areanuova <areanuova@unifg.it>
2026-06-06 10:19:40 +0200

    Aggiungi sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   // Cambia questo indirizzo con il tuo dominio definitivo (serve per SEO e sitemap).
-  site: 'https://www.areanuova.it',
-  integrations: [tailwind()],
+  site: 'https://area-nuova.vercel.app',
+  integrations: [tailwind(), sitemap()],
 });
